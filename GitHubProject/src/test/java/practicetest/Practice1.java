9297a190525fce9bf0166d3b0b8a15ee703bc8db
Admin <Admin@DESKTOP-SUQEQEJ>
2026-01-17 11:38:11 +0530

    made changea

--- GitHubProject/src/test/java/practicetest/Practice1.java
+++ GitHubProject/src/test/java/practicetest/Practice1.java
@@ -1,5 +1,15 @@
 package practicetest;
 
-public class Practice1 {
 
+public class Practice1 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("changes done  ....");
+	}
+
+	
+	
+	
 }
